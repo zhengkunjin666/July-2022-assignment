9be922299cf446eb3badff40118d2ec95c4324f8
zhengkunjin <zhengkunjin@126.com>
2022-07-12 14:15:27 +0800

    refresh lessonCount

--- day6/lessonCount/lessonCount.js
+++ day6/lessonCount/lessonCount.js
@@ -32,23 +32,19 @@ const Series = [
 ];
 // 计算每个系列下有多少门课
 // return [{ name: 'Web', count: 4}, { name: 'PHP', count: 1}]
-// function lessonCount(datas){
-//     lessonCount=datas.map(elem => {
-//         if(elem.courses.length>1){
-//             count=elem.courses.reduce((a,b) => {
-//                 return a.lessons.length+b.lessons.length;
-//             });
-//         }else{
-//             count=elem.courses[0].lessons.length;
-//         }
-//         return {name: elem.name,count: counts};
-//     })
-//     console.log(lessonCount);
-// };
-// lessonCount(Series);
 
+// 方法一：
+function lessonCount(datas){
+    let result=datas.map(elem => {
+        let counts=elem.courses.map(elem => elem.lessons.length).reduce((a,b) => a+b);
+        return {name: elem.name,count: counts};
+    })
+    return result;
+};
+console.log(lessonCount(Series));
+// // 方法二：
 // function lessonCount(datas){
-//     lessonCount=datas.map(elem => {
+//     let result=datas.map(elem => {
 //         let counts=0;
 //         for(let i=0;i<elem.courses.length;i++){
 //             let count=elem.courses[i].lessons.length;
@@ -56,19 +52,19 @@ const Series = [
 //         }
 //         return {name: elem.name,count: counts};
 //     });
-//     console.log(lessonCount);
+//     return result;
 // }
-// lessonCount(Series);
-
-function lessonCount(datas){
-    lessonCount=datas.map(elem => {
-        let counts=0;
-        elem.courses.forEach(elem => {
-            let count=elem.lessons.length;
-            counts +=count;
-        });
-        return {name: elem.name,count: counts};
-    });
-    console.log(lessonCount);
-}
-lessonCount(Series);
+// console.log(lessonCount(Series));
+// 方法三：
+// function lessonCount(datas){
+//     let result=datas.map(elem => {
+//         let counts=0;
+//         elem.courses.forEach(elem => {
+//             let count=elem.lessons.length;
+//             counts +=count;
+//         });
+//         return {name: elem.name,count: counts};
+//     });
+//     return result;
+// }
+// console.log(lessonCount(Series));
